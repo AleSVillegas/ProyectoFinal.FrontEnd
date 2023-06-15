47c65d84ba0c98cafac45232afa022e714a948b4
Sebas Villegas <asebasvillegas@gmail.com>
2023-06-14 22:31:56 -0300

    Actualizaciones y correcciones de estilo y diseño.

--- my-app/src/app/service/educacion.service.ts
+++ my-app/src/app/service/educacion.service.ts
@@ -2,15 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Educacion } from '../model/educacion';
-//import { environment } from 'src/environments/environment.prod';
 import { environment } from 'src/environments/environment';
+//import { environment } from 'src/environments/environment.prod';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EducacionService {
   
-  url: string = environment.URL + 'skill/';
+  url: string = environment.URL + 'educacion/';
 
   constructor(private httpClient : HttpClient) { }
 
